3ed1f29edff6571ace93b347cef11b233405e19c
Manexsora <manexsora@gmail.com>
2024-02-12 16:26:49 +0200

    Homework 2

---
+++ Assets/Scripts/Switch cameras.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Switchcameras : MonoBehaviour
+{
+    private Vector3 roomPos = new Vector3(0,5.22f,-1.19f);
+    private Quaternion roomRot = new Quaternion(0,180,0,0);
+    public Transform externalTransform;  // Set this in the Unity Editor to the external viewing point position
+    private bool isInRoom = true;
+    public InputActionReference action;
+
+
+    void Start()
+    {
+        transform.position = roomPos;
+        transform.rotation = roomRot;
+        action.action.Enable();
+    }
+    void Update()
+    {
+        action.action.Enable();
+        action.action.performed += (ctx) =>
+        {
+        if (isInRoom)
+            {
+                transform.position = externalTransform.position;
+                transform.rotation = externalTransform.rotation;
+            }
+        else
+            {
+                transform.position = roomPos;
+                transform.rotation = roomRot;
+            }
+
+        isInRoom = !isInRoom;
+        };
+    }
+
+}
---
+++ Assets/Scripts/cameraRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotation : MonoBehaviour
+{
+    public Camera mainCamera;
+    void Update()
+    {
+        Vector3 lensPosition = transform.position;
+        Vector3 cameraPosition = mainCamera.transform.position;
+
+        Vector3 lookDirection = lensPosition - cameraPosition;
+
+        Quaternion finalRotation = Quaternion.LookRotation(lookDirection);
+
+        transform.rotation = finalRotation;
+    }
+
+}
