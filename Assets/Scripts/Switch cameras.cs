9c452be5964b2cb7f395d33c9007d739fac9c4dc
Manex Sorarrain Agirrezabala <msorarra23@student.oulu.fi>
2024-02-13 12:06:51 +0200

    Homework 2

--- Assets/Scripts/Switch cameras.cs
+++ Assets/Scripts/Switch cameras.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Switchcameras : MonoBehaviour
 {
-    private Vector3 roomPos = new Vector3(0,0,1.9f);
+    private Vector3 roomPos = new Vector3(0,1.67f, 1.08f);
     private Quaternion roomRot = new Quaternion(0,180,0,0);
     public Transform externalTransform;  // Set this in the Unity Editor to the external viewing point position
     private bool isInRoom = true;
